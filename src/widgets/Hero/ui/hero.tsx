8717812aa104b13a6eed72005a1b1449f41d03ba
Kamo Mkoyan <kamomkoyan>
2025-03-03 11:39:06 +0400

    fix bugs

--- src/widgets/Hero/ui/hero.tsx
+++ src/widgets/Hero/ui/hero.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
-import {FileUploader} from "@/features/fileUpload";
 
 export function Hero() {
     return (
